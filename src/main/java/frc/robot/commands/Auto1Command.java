de489028b416c2fb5be6cfa74936d62b68170afd
kbyquist <kasey.byquist@icloud.com>
2022-10-27 18:00:48 -0700

    PathPlanner 2023.0.1 Update

--- src/main/java/frc/robot/commands/Auto1Command.java
+++ src/main/java/frc/robot/commands/Auto1Command.java
@@ -7,14 +7,19 @@ package frc.robot.commands;
 import static frc.robot.Constants.AutoConstants.*;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.PathPlanner;
 import java.util.ArrayList;
 import com.pathplanner.lib.PathConstraints;
+
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 
 
 
@@ -35,10 +40,13 @@ public class Auto1Command extends SequentialCommandGroup {
    * @param driveSubsystem The drive subsystem this command will run on
    * @param intakeSubsystem The hatch subsystem this command will run on
    */
-  public Auto1Command(DriveSubsystem driveSubsystem, IntakeSubsystem intakeSubsystem) {
+  public Auto1Command(DriveSubsystem driveSubsystem, IntakeSubsystem intakeSubsystem, ShooterSubsystem shooterSubsystem) {
     addCommands(
       driveSubsystem.followTrajectoryCommand(pathGroup1.get(0), true),
-      new WaitCommand(3),
+      new ParallelRaceGroup(
+        new WaitCommand(pathGroup1.get(0).getEndWaitTimeSeconds()),
+        new RunCommand(() -> shooterSubsystem.shoot(), shooterSubsystem)
+      ),
       driveSubsystem.followTrajectoryCommand(pathGroup1.get(1), false)
     );
     
